f0c00987409380adb9983fdd09b533cf4476f137
Muhammad HuzaiFa <78421415+HuzaiFa3030@users.noreply.github.com>
2022-11-11 17:50:13 +0500

    Update DBConfig.js

--- SQL/DBConfig.js
+++ SQL/DBConfig.js
@@ -1,11 +1,11 @@
 const dbconfig = {
-  HOST: "",       // Enter your host name
-  server: "",     // Enter your server name
-  user: "",       //Enter your username
-  password: "",   //Enter your password
-  database: "",   //Enter your database name
-  port: 1433,     //defult port 1433
-  dialect: "mssql",   //defult
+  HOST: "",       // Enter your host name /like localhost.
+  server: "",     // Enter your server name.
+  user: "",       //Enter your username.
+  password: "",   //Enter your password.
+  database: "",   //Enter your database name.
+  port: 1433,     //defult port 1433.
+  dialect: "mssql",   //defult.
   options: {
     trustedconnection: true,
     enableArithAbort: true,
